f3b60c22633363636f5909828a1f4d38176b8e1f
SilverBeamx <molto.falso@gmail.com>
2024-03-24 22:40:38 +0100

    Add threaded bloom test

---
+++ testThreadedBloom.cpp
@@ -0,0 +1,20 @@
+#include "threadedBloom.h"
+
+#include <iostream>
+#include <vector>
+
+int main(){
+    std::vector<const char*> testList{"this", "that", "thot", "test"};
+    ThreadedBloom* tb = new ThreadedBloom(1234, 3);
+
+    tb->addFilter((unsigned char*)"test", 4);
+    auto result = tb->isProbablyPresent(testList);
+
+    for (int i = 0; i < result.size(); ++i){
+        std::cout << result[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
+
+}
