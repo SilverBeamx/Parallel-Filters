149ad7c2ea5679cb8438c2769fb9f90e9636c2cb
SilverBeamx <molto.falso@gmail.com>
2024-04-05 09:53:10 +0200

    Add ThreadedBloomTimng test

---
+++ testThreadedBloomTiming.cpp
@@ -0,0 +1,33 @@
+#include "threadedBloom.h"
+#include "utils.h"
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(){
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+    using std::chrono::microseconds;
+
+    std::vector<std::string> lineVector;
+    readFileByLine("./generate_input_script/fruitList.txt", lineVector);
+
+    ThreadedBloom* tb = new ThreadedBloom(1234, 12);
+
+    tb->addFilter((unsigned char*)"test", 4);
+
+    auto t1 = high_resolution_clock::now();
+    auto result = tb->isProbablyPresent(lineVector);
+    auto t2 = high_resolution_clock::now();
+    auto time_elapsed = duration_cast<microseconds>(t2 - t1);
+
+    std::cout << "Elapsed filter find time: " << time_elapsed.count() << " us" << std::endl;
+
+    return 0;
+
+}
