ba88c16bb1356a34a8c41fc6e3c3f00c76052c78
SilverBeamx <molto.falso@gmail.com>
2024-04-05 10:21:39 +0200

    Remove leftover code from utils

--- utils.cpp
+++ utils.cpp
@@ -9,9 +9,7 @@ void readFileByLine(std::string fileName, std::vector<std::string>& lineVector){
     std::ifstream file(fileName);
     std::string line;
 
-    while (std::getline(file, line))
-    {
-        //std::cout << line.c_str() << std::endl;
+    while (std::getline(file, line)){
         lineVector.push_back(line);
     }
 }
