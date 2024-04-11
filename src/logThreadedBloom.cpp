270bff5f93d0f0587b706eda44cc7ea1ea680342
SilverBeamx <molto.falso@gmail.com>
2024-04-11 15:48:01 +0200

    Migrate to steady_clock

--- src/logThreadedBloom.cpp
+++ src/logThreadedBloom.cpp
@@ -12,7 +12,7 @@
 using namespace args;
 
 int main(int argc, char **argv){
-    using std::chrono::high_resolution_clock;
+    using std::chrono::steady_clock;
     using std::chrono::duration_cast;
     using std::chrono::duration;
     using std::chrono::milliseconds;
@@ -29,17 +29,17 @@ int main(int argc, char **argv){
     std::vector<std::string> lineVector;
     readFileByLine("./scripts/fruitList.txt", lineVector);
 
-    auto t1 = high_resolution_clock::now();
+    auto t1 = steady_clock::now();
 
     ThreadedBloom* tb = new ThreadedBloom(1234, numThreads);
 
     tb->addFilter((unsigned char*)"test", 4);
 
     auto result = tb->isProbablyPresent(lineVector);
-    auto t2 = high_resolution_clock::now();
+    auto t2 = steady_clock::now();
     auto time_elapsed = duration_cast<milliseconds>(t2 - t1);
 
-    std::cout << "Elapsed filter find time: " << time_elapsed.count() << " us" << std::endl;
+    std::cout << "Elapsed filter find time: " << time_elapsed.count() << " ms" << std::endl;
 
     if(cmd_parser.found("log")){
         std::ofstream output("log/threadedTiming.csv", std::ios_base::app);
